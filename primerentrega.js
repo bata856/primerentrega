d21e3cf1c70734ea80a3c7b9902bd8ec8a9b6da3
juan jose batalla <bata856@gmail.com>
2023-03-12 15:29:19 -0300

    Entrega de la primera pre entrega

---
+++ primerentrega.js
@@ -0,0 +1,48 @@
+function saludar ()
+
+    {
+    let nombre = prompt ("Ingrese su nombre");
+    alert("Bienvenido" + "  "+  nombre + "!!");
+    } 
+
+saludar();
+
+let trio  = 3000;
+let riera = 4000;
+let terepin = 2000;
+let acumulador = 0;
+
+let mensaje = prompt ("¿Desea realizar una compra? (S-SI / N-NO)");
+    while (mensaje == "S" || mensaje == "s") 
+    {
+        let producto = prompt("1-Galletitas Trio \n2- Tostadas Riera \n3- Pepas Terepín");
+                                    if (producto == "1")    
+                                    {  
+                                    alert("Galletitas Trio ---- $3000 + IVA agregado a su carro");
+                                    acumulador= acumulador+trio;
+                                    }
+                                    else if (producto == "2")
+                                    
+                                    {
+                                    alert("Tostadas Riera ---- $4000 + IVA agregado a su carro");
+                                    acumulador= acumulador + riera;
+                                    }
+                                    else if (producto == "3")
+                                    {
+                                    alert("Pepas Terepín ---- $2000 + IVA agregado a su carro");
+                                    acumulador= acumulador + terepin;
+                                    }
+                                    else 
+                                    {
+                                    alert("Producto inexistente")
+                                    }
+        mensaje = prompt ("¿Desea comprar otro producto? (S-SI / N-NO)");
+        alert ("El valor de su compra es" + " $ " + acumulador + " +IVA");
+    }
+
+function calcularTotalConIva (totalCompra)
+{
+    let totalConIva = acumulador *1.21;
+    alert ("El precio final de su compra es" + " $" +  totalConIva + " " + "Gracias por su compra!!");
+}
+calcularTotalConIva();
